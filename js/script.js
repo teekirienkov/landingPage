7aa602c14b3bff22f4d67445166c8125a904094a
Timur681 <teekirienkov@gmail.com>
2020-03-29 17:26:39 +0300

    add for while

--- js/script.js
+++ js/script.js
@@ -114,6 +114,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
     // Form
+    // Отправка данных POST методом возможно только на локальном сервере!
 
     let message = {
         loading: 'Загрузка...',
@@ -147,7 +148,13 @@ window.addEventListener('DOMContentLoaded', () => {
                 statusMessage.innerHTML = message.loading;
             } else if (request.readyState === 4 && request.status == 200) {
                 statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
             }
         });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
     });
 });
