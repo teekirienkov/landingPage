e4ad61b58473f5efb26c89a9e8bc8ff5780f2bff
Timur681 <teekirienkov@gmail.com>
2020-04-01 19:45:59 +0300

    add promise

--- js/script.js
+++ js/script.js
@@ -124,10 +124,60 @@ window.addEventListener('DOMContentLoaded', () => {
 
     let form = document.querySelector('.main-form'), // получение формы обратной связи
         input = form.getElementsByTagName('input'),
+        formDown = document.querySelector('#form'),
         statusMessage = document.createElement('div'); // создание дива с сообщением
-
         statusMessage.classList.add('status'); // Этот класс уже прописан в css
 
+    
+    function sendForm(elem) {
+      elem.addEventListener('submit', function(event) {
+        event.preventDefault();
+        elem.appendChild(statusMessage);
+
+        let formData = new FormData(elem);
+
+      function postData() {
+          return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest();
+
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+            request.onreadystatechange = function() {
+                if (request.readyState < 4) {
+                    resolve();
+                } else if (request.readyState === 4) {
+                    if (request.status == 200 && request.status < 300) {
+                        resolve();
+                    } else {
+                        reject();
+                    }
+                }
+            };
+          });
+      }
+
+
+
+
+
+      });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/*
     form.addEventListener('submit', function(event) { // submit вешается не на кнопку а на форму!
         event.preventDefault();                 // отмена перезагрузки страницы при нажатии на кнопку
         form.appendChild(statusMessage);       // добавление в форму див
@@ -168,5 +218,4 @@ window.addEventListener('DOMContentLoaded', () => {
         for (let i = 0; i < input.length; i++) {
             input[i].value = '';
         }
-    });
-});
+*/
