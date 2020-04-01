6149f481f34e282a7e24818198dbbf015d81fb2e
Timur681 <teekirienkov@gmail.com>
2020-04-01 19:50:08 +0300

    full promise

--- js/script.js
+++ js/script.js
@@ -137,25 +137,31 @@ window.addEventListener('DOMContentLoaded', () => {
         let formData = new FormData(elem);
 
       function postData() {
-          return new Promise(function (resolve, reject) {
-            let request = new XMLHttpRequest();
+        return new Promise(function (resolve, reject) {
+          let request = new XMLHttpRequest();
 
-            request.open('POST', 'server.php');
+          request.open('POST', 'server.php');
 
-            request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+          request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
 
-            request.onreadystatechange = function() {
-                if (request.readyState < 4) {
-                    resolve();
-                } else if (request.readyState === 4) {
-                    if (request.status == 200 && request.status < 300) {
-                        resolve();
-                    } else {
-                        reject();
-                    }
-                }
+          request.onreadystatechange = function() {
+            if (request.readyState < 4) {
+              resolve();
+            } else if (request.readyState === 4) {
+                if (request.status == 200 && request.status < 300) {
+                  resolve();
+                } else {
+                    reject();
+                  }
+              }
             };
+          let obj = {};
+          formData.forEach (function (value, key) {
+            obj[key] = value;
           });
+          let json = JSON.stringify(obj);
+          request.send(json);
+        });
       }
 
 
