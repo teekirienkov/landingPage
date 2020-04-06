d5ba5ccd3a8556d8ac3eb38a8d4e489bcbd889a0
Timur681 <teekirienkov@gmail.com>
2020-04-06 16:26:46 +0300

    Немного поправил код

--- js/script.js
+++ js/script.js
@@ -258,40 +258,40 @@ window.addEventListener('DOMContentLoaded', () => {
       daysSum = 0,
       total = 0;
 
+  totalValue.innerHTML = 0;
+
+  // Количество человек
+  persons.addEventListener('change', function() {
+      personsSum = +this.value; // получаем value у persons (так как родитель в обработчике события)
+      total = (daysSum + personsSum)*4000;
+
+      // проверка, если второй инпут (кол-во дней) пустой, то цена поездки не изменяется, остаётся 0
+      if (restDays.value == '') {
+        totalValue.innerHTML = 0;
+      } else {
+          totalValue.innerHTML = total;
+      }
+  });
+  // Количество дней
+  restDays.addEventListener('change', function() {
+    daysSum = +this.value; // получаем value у persons (так как родитель в обработчике события)
+    total = (daysSum + personsSum)*4000;
+
+    // проверка, если второй инпут (кол-во дней) пустой, то цена поездки не изменяется, остаётся 0
+    if (persons.value == '') {
       totalValue.innerHTML = 0;
-
-      // Количество человек
-      persons.addEventListener('change', function() {
-          personsSum = +this.value; // получаем value у persons (так как родитель в обработчике события)
-          total = (daysSum + personsSum)*4000;
-
-          // проверка, если второй инпут (кол-во дней) пустой, то цена поездки не изменяется, остаётся 0
-          if (restDays.value == '') {
-            totalValue.innerHTML = 0;
-          } else {
-              totalValue.innerHTML = total;
-          }
-      });
-      // Количество дней
-      restDays.addEventListener('change', function() {
-        daysSum = +this.value; // получаем value у persons (так как родитель в обработчике события)
-        total = (daysSum + personsSum)*4000;
-
-        // проверка, если второй инпут (кол-во дней) пустой, то цена поездки не изменяется, остаётся 0
-        if (persons.value == '') {
-          totalValue.innerHTML = 0;
-        } else {
-            totalValue.innerHTML = total;
-        }
-      });
-      // чекбокс
-      place.addEventListener('change', function() {
-          if (restDays.value == '' || persons.value == '') {
-            totalValue.innerHTML = 0;
-          } else {
-              let a = total;
-              totalValue.innerHTML = a * this.options[this.selectedIndex].value; // получение из чекбокса value
-          }
-      }); // end calculator
+    } else {
+        totalValue.innerHTML = total;
+    }
+  });
+  // чекбокс
+  place.addEventListener('change', function() {
+      if (restDays.value == '' || persons.value == '') {
+        totalValue.innerHTML = 0;
+      } else {
+          let a = total;
+          totalValue.innerHTML = a * this.options[this.selectedIndex].value; // получение из чекбокса value
+      }
+  }); // end calculator
 
 }); // window
